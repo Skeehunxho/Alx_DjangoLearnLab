d2dc53757805c446143e4f4cde70f2025ea51a32
Skeehunxho <Kentmmmuia18@gmail.com>
2024-07-14 22:09:43 +0300

    Add new files

---
+++ ALX_WORK/quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+    // function body
+    var correctAnswer = "4";
+    
+    // Retrieve the User’s Answer
+    var usersAnswer=document.querySelector('input[name="quiz"]:checked').value;
+  
+    // Compare the User’s Answer with the Correct Answer
+    if (userAnswer === correctAnswer) {
+      document.getElementById('feedback').textContent='"Correct! Well done."'
+    }
+    else { 
+      document.getElementById('feedback').textContent= "That's incorrect. Try again!"
+    }
+  
+  }
+  
+    // Add an Event Listener to the Submit Button
+    document.getElementById('submit-answer').addEventListener('click', checkAnswer);

585fe5d330099b637f691eabe97be65ced458648
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-18 12:34:51 +0300

    Update Changes

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        "message": "Welcome to Advanced API Project 🚀",
+        "endpoints": {
+            "books": "/api/books/",
+            "authors": "/api/authors/"
+        }
+    })
 
 urlpatterns = [
+    path("", api_root),  # root endpoint
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
+

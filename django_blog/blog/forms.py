45534fc6238fab25c8f362132248b7d321a0916b
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-18 13:52:17 +0300

    Update All Changes

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -9,14 +9,17 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         
-# blog/forms.py
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget  
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']  
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
 from django import forms
 from .models import Comment
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -16,27 +16,27 @@ class Post(models.Model):
 
 from django.db import models
 from django.contrib.auth.models import User
-
-class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
-
-from django.db import models
-from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = TaggableManager()   
     created_at = models.DateTimeField(auto_now_add=True)
-    from taggit.managers import TaggableManager
-    tags = TaggableManager()  
+    updated_at = models.DateTimeField(auto_now=True)
 
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')  
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'

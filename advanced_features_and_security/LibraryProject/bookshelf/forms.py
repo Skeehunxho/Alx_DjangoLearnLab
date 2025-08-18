f18ed1c4aced5c20774ba216756d4c6ffd49f02a
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-18 11:15:04 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,3 @@
-# relationship_app/forms.py
 from django import forms
 from ..relationship_app.models import Book
 
@@ -6,3 +5,18 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date', 'isbn', 'genre']
+        
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your name'})
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Enter your message'}),
+        required=True
+    )
